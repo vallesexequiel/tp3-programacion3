3d8e048faccb1c36c35bd7424fdc0adbf6a75d06
valles exequiel matias <exequielvalles21@gmai.com>
2025-06-26 20:22:30 -0300

    uso useEffect para llamar a la api y fech para obtener informacion del servidor

--- ejercicio3/src/App.jsx
+++ ejercicio3/src/App.jsx
@@ -2,4 +2,18 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [tareas, setTareas] = useState([]);
+ useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(data => {
+        const tareasCompletadas = data
+          .filter(tarea => tarea.completed)
+          .map(tarea => ({
+            ...tarea,
+            title: `✅ Tarea completada #${tarea.id} del usuario ${tarea.userId}`
+          }));
+        setTareas(tareasCompletadas);
+      })
+      .catch(error => console.error('Error al obtener tareas:', error));
+  }, []);
 }
