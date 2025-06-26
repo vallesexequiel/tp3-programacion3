9db018df13c392a8039300cf7d1236d2588bc322
valles exequiel matias <exequielvalles21@gmai.com>
2025-06-26 18:24:55 -0300

    constructores

---
+++ ejercicio1/scrip.js
@@ -0,0 +1,5 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const operacion = document.getElementById("operacion");
+const calcularBtn = document.getElementById("calcular");
+const resultado = document.getElementById("resultado");
