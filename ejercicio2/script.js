4c9593437a1756dfb8ce6843583b2af985a70847
valles exequiel matias <exequielvalles21@gmai.com>
2025-06-26 18:41:59 -0300

    agrego los primeros constructores}

---
+++ ejercicio2/script.js
@@ -0,0 +1,6 @@
+const palabras = ["manzana", "banana", "pera", "durazno", "frutilla", "mango"];
+
+const filtroInput = document.getElementById("filtro");
+const filtrarBtn = document.getElementById("filtrarBtn");
+const resultado = document.getElementById("resultado");
+const mensajeError = document.getElementById("mensajeError");
