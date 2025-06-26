9592d17421c78abd505be3897c5967d52eb0f00e
valles exequiel matias <exequielvalles21@gmai.com>
2025-06-26 18:45:58 -0300

    agrego el filtro

--- ejercicio2/script.js
+++ ejercicio2/script.js
@@ -4,3 +4,14 @@ const filtroInput = document.getElementById("filtro");
 const filtrarBtn = document.getElementById("filtrarBtn");
 const resultado = document.getElementById("resultado");
 const mensajeError = document.getElementById("mensajeError");
+
+filtrarBtn.addEventListener("click", () => {
+  const texto = filtroInput.value.trim().toLowerCase();
+
+   // Limpiar resultado anterior
+  resultado.innerHTML = "";    
+  
+  // Limpiar error anterior
+  mensajeError.textContent = "";
+
+});
