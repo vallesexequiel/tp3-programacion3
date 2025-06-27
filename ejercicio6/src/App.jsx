14dcffe174c7adfa0cc5665104774d669b234c4c
valles exequiel matias <exequielvalles21@gmai.com>
2025-06-26 22:55:56 -0300

    Agregar inputs controlados para peso y altura

--- ejercicio6/src/App.jsx
+++ ejercicio6/src/App.jsx
@@ -7,4 +7,37 @@ function App() {
   const [mensaje, setMensaje] = useState('');
   const [color, setColor] = useState('');
 
+  return(
+       <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h2>Calculadora de IMC</h2>
+
+      <input
+        type="number"
+        placeholder="Peso (kg)"
+        value={peso}
+        onChange={(e) => setPeso(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="number"
+        placeholder="Altura (m)"
+        value={altura}
+        onChange={(e) => setAltura(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={calcularIMC}>Calcular IMC</button>
+      <br /><br />
+
+      {imc && (
+        <div style={{ color, fontWeight: 'bold' }}>
+          Tu IMC es {imc} - {mensaje}
+        </div>
+      )}
+    </div>
+
+
+  );
+
 }
