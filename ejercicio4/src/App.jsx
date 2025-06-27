8eb1953980740981c6ab7b7816d4591b9a258ba7
valles exequiel matias <exequielvalles21@gmai.com>
2025-06-26 21:20:05 -0300

    el boton izquierdo solo se deshabilita cuando demos click en boton derecho

--- ejercicio4/src/App.jsx
+++ ejercicio4/src/App.jsx
@@ -2,4 +2,16 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function BotonesAlternados() {
-  const [habilitado, setHabilitado] = useState('izquierdo');}
+  const [habilitado, setHabilitado] = useState('izquierdo');
+    return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <button
+        onClick={() => setHabilitado('derecho')}
+        disabled={habilitado !== 'izquierdo'}
+      >
+        Izquierdo
+      </button>
+
+    </div>
+  );
+}
