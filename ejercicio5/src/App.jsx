46ec81e165cb95243eb5f7ce6174599a9eddd82f
valles exequiel matias <exequielvalles21@gmai.com>
2025-06-26 22:13:46 -0300

    agrego la funcion que calcula el resultado

--- ejercicio5/src/App.jsx
+++ ejercicio5/src/App.jsx
@@ -4,4 +4,24 @@ function App() {
   const [num1, setNum1] = useState("");
   const [num2, setNum2] = useState("");
   const [operacion, setOperacion] = useState("suma");
-  const [resultado, setResultado] = useState(null);}
+  const [resultado, setResultado] = useState(null);
+
+const calcular = () => {
+    const a = parseFloat(num1);
+    const b = parseFloat(num2);
+    let res = "";
+
+    if (operacion === "suma") {
+      res = a + b;
+    } else if (operacion === "resta") {
+      res = a - b;
+    } else if (operacion === "multiplicacion") {
+      res = a * b;
+    } else {
+      res = "Operación inválida";
+    }
+
+    setResultado(res);
+  };
+
+}
