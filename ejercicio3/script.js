e07d461181122dbd1e1a906f8e69abb6be939758
valles exequiel matias <exequielvalles21@gmai.com>
2025-06-29 22:33:04 -0300

    hacemos la peticion a la api y mostramos el listado en una ul

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -1,3 +1,23 @@
 
 window.addEventListener('DOMContentLoaded', () => {
+
+     
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(data => {
+
+      const completadas = data.filter(tarea => tarea.completed === true);
+
+      const lista = document.getElementById('lista');
+
+        completadas.forEach(tarea => {
+            const li = document.createElement('li');
+            li.textContent = `✅ Tarea completada #${tarea.id} del usuario ${tarea.userId}`;
+            lista.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.error('Error al obtener las tareas:', error);
+    });
+
 });
